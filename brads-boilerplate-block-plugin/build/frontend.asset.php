2c421351a421094c380d7e1b0294031a32a34e48
Tom Myruski <thomas.myruski@gmail.com>
2024-11-15 13:16:35 -0500

    Test subfolder

---
+++ brads-boilerplate-block-plugin/build/frontend.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'react-dom'), 'version' => '3768d06322b798f82a39');
---
+++ brads-boilerplate-block-plugin/build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks'), 'version' => '5041e37a4996df986cea');
