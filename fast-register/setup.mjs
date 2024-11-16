ad3581c6c7186f771ca3f6b2c30d524be09bbb1f
Tom Myruski <thomas.myruski@gmail.com>
2024-11-16 12:59:46 -0500

    Ability to add CSS

--- fast-register/setup.mjs
+++ fast-register/setup.mjs
@@ -2,9 +2,16 @@ import fs from 'fs';
 import { input, select } from '@inquirer/prompts';
 
 const filePath = 'resources/modules/fast-register/js'; // Adjust this path as needed
+const cssPath = 'resources/modules/fast-register/css'; // Adjust this path as needed
 
 async function main() {
-  const jsFiles = fs.readdirSync('js').filter(file => file.endsWith('.js'));
+  if (!fs.existsSync('js')) {
+
+  }
+
+  const jsFiles = fs.existsSync('js') ? fs.readdirSync('js').filter(file => file.endsWith('.js')) : [];
+  const cssFiles =  fs.existsSync('css') ?  fs.readdirSync('css').filter(file => file.endsWith('.css')) : [];
+
 
   // Check if index.php exists, create it if not
   if (!fs.existsSync('index.php')) {
@@ -70,6 +77,38 @@ add_action('wp_enqueue_scripts', 'boilerplate_load_assets');
     fs.writeFileSync('index.php', indexPhpContent);
 
     console.log(`Script '${fileName}' registered as '${registrationName}' successfully.`);
+  }
+  for (const file of cssFiles) {
+    const fileName = file.replace('.css', '');
+
+    // Check if the style is already registered in index.php
+    if (indexPhpContent.includes(`'${fileName}'`)) {
+      console.log(`Style '${fileName}' is already registered.`);
+      continue;
+    }
+
+    // Ask for the registration name
+    const registrationName = await input({
+      message: `Enter the registration name for '${fileName}':`,
+      default: fileName,
+    });
+
+
+    // Create the registration code
+    const registrationCode = `
+    wp_register_style('${registrationName}', get_theme_file_uri('/${cssPath}/${file}') );
+`;
+
+    // Insert the registration code before the closing brace
+    indexPhpContent = indexPhpContent.slice(0, closingBraceIndex) + registrationCode + indexPhpContent.slice(closingBraceIndex);
+
+    // Update the closing brace index for the next iteration
+    closingBraceIndex += registrationCode.length;
+
+    // Write the modified content back to the file
+    fs.writeFileSync('index.php', indexPhpContent);
+
+    console.log(`Style '${fileName}' registered as '${registrationName}' successfully.`);
   }
 }
 
